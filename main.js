cf215289de632cee3b9d95869c6b044301d411e3
ipaullly <ipaully22@gmail.com>
2020-01-22 23:30:29 +0300

    Refactor function logic to class-based approach

--- main.js
+++ main.js
@@ -1,24 +1,5 @@
-let config = {
-  type: Phaser.AUTO,
-  width: 600,
-  height: 400,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 300 },
-      debug: false
-    }
-  },
-  scene: {
-    preload,
-    create,
-    update,
-  }
-};
-
-let game = new Phaser.Game(config);
 // game variables
-
+let game;
 let bird;
 let cursors;
 let bottom_pipe = [];
@@ -28,63 +9,138 @@ let endGame;
 let distance;
 initialYAxisOffset = [];
 
-const preload = () => {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-  this.load.image('pipe', 'assets/pipe.png');
+window.onload = () => {
+  let config = {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 400,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 300 },
+        debug: false
+      }
+    },
+    scene: [playGame],
+  };
+  
+  game = new Phaser.Game(config);
+  // window.focus();
+  // resize();
+  // window.addEventListener('resize', resize, false);
 }
 
-const create = () => {
-  // add sky
-  this.add.image(400, 300, 'sky');
-  // add flappy bird
-  bird = this.physics.add.sprite(20, 40, 'bird');
-  // add collision detection
-  bird.setBounce(0.2);
-  bird.setCollideWorldBounds(true);
-  bird.body.setGravityY(300);
+class playGame extends Phaser.Scene {
+  constructor(){
+    super('PlayGame');
+  }
 
-  // for-loop to create three pipes
-  let xPos = 0;
-  let firstRandomNumber;
-  for (let i=0;i<3;i++) {
-    firstRandomNumber = (Math.floor(Math.random() * 10)) + 1;
-    if (firstRandomNumber < 6) {
-      // add random positive number to pipe's new y position
-      initialYAxisOffset[i] = (Math.floor(Math.random() * 10)) * 10;
+  preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('bird', 'assets/bird.png');
+    this.load.image('pipe', 'assets/pipe.png');
+  }
+  create() {
+     // add sky
+    this.add.image(400, 300, 'sky');
+    // add flappy bird
+    bird = this.physics.add.sprite(20, 40, 'bird');
+    // add collision detection
+    bird.setBounce(0.2);
+    bird.setCollideWorldBounds(true);
+    bird.body.setGravityY(300);
+
+    // for-loop to create three pipes
+    this.xPos = 0;
+    this.firstRandomNumber;
+    for (let i=0;i<3;i++) {
+      this.firstRandomNumber = (Math.floor(Math.random() * 10)) + 1;
+      if (this.firstRandomNumber < 6) {
+        // add random positive number to pipe's new y position
+        initialYAxisOffset[i] = (Math.floor(Math.random() * 10)) * 10;
+      } else {
+        // add random negative number to pipe's new y position
+        initialYAxisOffset[i] = (Math.floor(Math.random() * 10)) * (-10);
+      }
+    }
+    this.offset;
+    // Create Top pipes
+    for (let j=0;j<3;j++) {
+      this.offset = initialYAxisOffset[j];
+      top_pipe[j] = this.physics.add.sprite(550 + this.xPos, -50 + this.offset, 'pipe' );
+      top_pipe[j].body.setAllowGravity(false);
+      top_pipe[j].body.setVelocityX(-150);
+      top_pipe[j].inableBody = true;
+      top_pipe[j].immovable = true;
+      top_pipe[j].flipY = true
+
+      this.xPos += 400;
+    }
+    // Create bottom pipes
+    this.xPos = 0; // reset xPos to default value
+    for(let i=0;i<3;i++) {
+      this.offset = initialYAxisOffset[i];
+      bottom_pipe[i] = this.physics.add.sprite(550 + this.xPos, 450 + this.offset, 'pipe' );
+      bottom_pipe[i].body.setAllowGravity(false);
+      bottom_pipe[i].body.setVelocityX(-150);
+      bottom_pipe[i].inableBody = true;
+      bottom_pipe[i].immovable = true;
+
+      this.xPos += 400;
+    }
+    this.onScreenText = this.add.text(212, 185, '', { fontSize: '32px', fill: '#00000' });
+    cursors = this.input.keyboard.createCursorKeys();
+    // add collection detection between bird and pipe(s)
+  }
+  xPos = 0;
+  update() {
+    if (this.physics.overlap(bird, top_pipe) || 
+      this.physics.overlap(bird, bottom_pipe)) {
+      this.collisionCallback();
+    }
+    if (gameOver) {
+      bird.body.setGravity(0);
+      for (let i=0;i<3;i++) {
+        bottom_pipe[i].body.setAllowGravity(false);
+        bottom_pipe[i].body.setVelocityX(0);
+        top_pipe[i].body.setGravity(false);
+        top_pipe[i].body.setVelocityX(0);
+      }
+      return;
     } else {
-      // add random negative number to pipe's new y position
-      initialYAxisOffset[i] = (Math.floor(Math.random() * 10)) * (-10);
+      if (cursors.space.isDown) {
+        bird.setVelocityY(-250);
+        console.log("Spacebar pressed, I rock"); 
+      }
+      for (let i=0;i<3;i++) {
+        this.firstRandomNumber = (Math.floor(Math.random() * 10)) + 1;
+        if (this.firstRandomNumber < 6) {
+          // add random positive number to pipe's new y position
+          this.yAxisOffset = (Math.floor(Math.random() * 10)) + 1;
+        } else {
+          // add random negative number to pipe's new y position
+          this.yAxisOffset = (Math.floor(Math.random() * 10)) * (-10);
+        }
+        if (bottom_pipe[i].body.x < -400) {
+          bottom_pipe[i].x = 850;
+          bottom_pipe[i].y = 450 + this.yAxisOffset;
+        }
+        if (top_pipe[i].body.x < -400) {
+          top_pipe[i].x = 850;
+          top_pipe[i].y = -50 + this.yAxisOffset;
+        }
+      }
     }
   }
-  let offset;
-  // Create Top pipes
-  for (let j=0;j<3;j++) {
-    offset = initialYAxisOffset[j];
-    top_pipe[j] = this.physics.add.sprite(550 + xPos, -50 + offset, 'pipe' );
-    top_pipe[j].body.setAllowGravity(false);
-    top_pipe[j].body.setVelocityX(-150);
-    top_pipe[j].inableBody = true;
-    top_pipe[j].immovable = true;
-    top_pipe[j].flipY = true
-
-    xPos += 400;
+  collisionCallback(){
+    gameOver = true;
+    bird.body.enable = false;
+    top_pipe[0].body.enable = false;
+    top_pipe[1].body.enable = false;
+    top_pipe[2].body.enable = false;
+    bottom_pipe[0].body.enable = false;
+    bottom_pipe[1].body.enable = false;
+    bottom_pipe[2].body.enable = false;
+    this.onScreenText.setText('GAME OVER');
   }
-  // Create bottom pipes
-  xPos = 0; // reset xPos to default value
-  for(let i=0;i<3;i++) {
-    offset = initialYAxisOffset[i];
-    bottom_pipe[i] = this.physics.add.sprite(550 + xPos, 450 + offset, 'pipe' );
-    bottom_pipe[i].body.setAllowGravity(false);
-    bottom_pipe[i].body.setVelocityX(-150);
-    bottom_pipe[i].inableBody = true;
-    bottom_pipe[i].immovable = true;
-
-    xPos += 400;
-  }
-  onScreenText = this.add.text(212, 185, '', { fontSize: '32px', fill: '#00000' });
-  cursors = this.input.keyboard.createCursorKeys();
-  // add collection detection between bird and pipe(s)
-  this.physics.add.collider(bird, bottom_pipe, collisionCallback);
-  this.physics.add.collider(bird, top_pipe, collisionCallback);
 }
